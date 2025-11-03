9c64595488c88bff09cfe4d19820cb2f10d9a505
David Gjorgoski <david_gjorgoski@hotmail.com>
2025-11-03 18:56:58 +0100

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const microservices = app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.REDIS,
+    options: {
+      host: process.env.REDIS_HOST || 'localhost',
+      port: parseInt(process.env.REDIS_PORT || '3333'),
+    },
+  });
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    credentials: true,
+  });
+
+  await app.startAllMicroservices();
+  await app.listen(process.env.PORT ?? 3001);
+}
+bootstrap();
